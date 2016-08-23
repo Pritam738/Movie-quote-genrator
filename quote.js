d96f561e7f3e66d12904e1c96bb0d08641f6600d
Pritam sen <spartian.sen738@gmail.com>
2016-08-23 18:25:10 +0530

    first commit

---
+++ quote.js
@@ -0,0 +1,239 @@
+var rnd;
+
+function PickRandomWord() {
+    // Generate a random number between 1 and NumberOfquotes
+    rnd = Math.floor((Math.random() * 58) + 1);
+    console.log(rnd);
+    switch (rnd) {
+        case 1:
+            quotes = "They may take our lives, but they'll never take our freedom! <br> :Braveheart";
+            break;
+        case 2:
+            quotes = "Elementary, my dear Watson <br>:Sherlock holmes";
+            break;
+        case 3:
+
+            quotes = "Oh, it was nobody's fault but my ownI was looking up It was the nearest thing to heaven You were there <br>:An Affair to Remember (1957)";
+
+            break;
+        case 4:
+
+            quotes = "When you realize you want to spend the rest of your life with somebody, you want the rest of your life to start as soon as possible<br> :When Harry Met Sally, 1989";
+
+            break;
+        case 5:
+
+            quotes = "Fatality!<br> :Mortal Combat ";
+            break;
+        case 6:
+
+            quotes = "If you let my daughter go now, that'll be the end of it I will not look for you, I will not pursue you But if you don't, I will look for you, I will find you, and I will kill you<br> :Taken, 2008";
+            break;
+        case 7:
+
+            quotes = "My name is Maximus Decimus Meridius, commander of the Armies of the North, General of the Felix Legions and loyal servant to the true emperor, Marcus Aurelius Father to a murdered son, husband to a murdered wife And I will have my vengeance, in this life or the next <br>:Gladiator, 2000";
+            break;
+        case 8:
+
+            quotes = "You make me want to be a better man<br> :As Good as It Gets, 1997";
+            break;
+        case 9:
+
+            quotes = "Chewie, we're home :Star Wars Episode VII: <br>The Force Awakens, 2015";
+            break;
+        case 10:
+
+            quotes = "They're here!<br> :Poltergeist";
+            break;
+        case 11:
+
+            quotes = "You complete me<br> :Jerry Maguire, 1996";
+            break;
+        case 12:
+
+            quotes = "Magic Mirror on the wall, who is the fairest one of all? <br> :Snow White and the Seven Dwarves";
+            break;
+        case 13:
+
+            quotes = "Just when I thought I was out, they pull me back in <br>:The Godfather ";
+            break;
+        case 14:
+
+            quotes = "Nobody's perfect<br> :Some Like It Hot";
+            break;
+        case 15:
+
+            quotes = "You ain't heard nothin' yet! <br>:The Jazz Singer";
+            break;
+        case 16:
+            quotes = "Gentlemen, you can't fight in here! This is the war room!<br>:How I Learned to Stop Worrying and Love the Bomb";
+            break;
+        case 17:
+            quotes = "I don't want to survive I want to live <br>:12 Years a Slave";
+            break;
+        case 18:
+            quotes = "Good morning, Vietnam! <br>:Good morning, Vietnam";
+            break;
+        case 19:
+            quotes = "My precious<br> :The Lord of the Rings";
+            break;
+        case 20:
+            quotes = "It's alive! It's alive! <br>:Frankenstein";
+            break;
+        case 21:
+            quotes = "You is kind You is smart You is important<br> :The Help";
+            break;
+        case 22:
+            quotes = "Go ahead, make my day <br>:Sudden Impact";
+            break;
+        case 23:
+
+            quotes = "After all, tomorrow is another day! <br>:Gone With the Wind";
+            break;
+        case 24:
+
+            quotes = "Pay no attention to that man behind the curtain! <br>:The Wizard of Oz";
+            break;
+        case 25:
+
+            quotes = "Hasta la vista, baby <br>:Terminator 2-Judgment Day";
+            break;
+        case 26:
+
+            quotes = "The Road to Isengard<br>:Lord of the Rings";
+            break;
+        case 27:
+
+            quotes = "I'm also just a girl, standing in front of a boy, asking him to love her <br>:Nothing Hill";
+            break;
+        case 28:
+
+            quotes = "I'll get you, my pretty, and your little dog, too! <br>:The Wizard of Oz";
+            break;
+        case 29:
+
+            quotes = "I'm having an old friend for dinner <br>:The Silence of the Lambs";
+            break;
+        case 30:
+
+            quotes = "I'm not bad I'm just drawn that way <br>:Who Framed Roger Rabbit";
+            break;
+        case 31:
+
+            quotes = "Mama says, 'Stupid is as stupid does'<br> :Forrest Gump";
+            break;
+        case 32:
+
+            quotes = "I'm the king of the world! <br>:Titanic";
+            break;
+        case 33:
+
+            quotes = "Shaken, not stirred <br>:James Bond ";
+            break;
+        case 34:
+
+            quotes = "Keep your friends close, but your enemies closer<br> :The Godfather";
+            break;
+        case 35:
+
+            quotes = "The greatest trick the devil ever pulled was convincing the world he didn't exist :The Usual Suspects";
+            break;
+        case 36:
+
+            quotes = "Fasten your seatbelts It's going to be a bumpy night<br> :All About Eve";
+            break;
+        case 37:
+
+            quotes = "I love the smell of napalm in the morning <br>:Apocalypse Now";
+            break;
+        case 38:
+
+            quotes = "Say hello to my little friend!<br> :Scarface";
+            break;
+        case 39:
+
+            quotes = "Show me the money! <br>:Jerry Maguire";
+            break;
+        case 40:
+
+            quotes = "Carpe diem Seize the day, boys <br>:Dead Poets Society";
+            break;
+        case 41:
+
+            quotes = "Here's Johnny!<br>:The Shining";
+            break;
+        case 42:
+
+            quotes = "Houston, we have a problem<br>:Apollo 13";
+            break;
+        case 43:
+
+            quotes = "To infinity and beyond! <br>:Toy Story";
+            break;
+        case 44:
+
+            quotes = "I'll be back <br>:The Terminator";
+            break;
+        case 45:
+
+            quotes = "I see dead people :The Sixth Sense";
+            break;
+        case 46:
+
+            quotes = "My name is Bond James Bond<br> :James Bond";
+            break;
+        case 47:
+
+            quotes = "This is the beginning of a beautiful friendship<br> :Casablanca";
+            break;
+        case 48:
+
+            quotes = "I'll have what she's having <br>:When Harry Met Sally";
+            break;
+        case 49:
+
+            quotes = "Why so serious? <br>:The Dark Knight";
+            break;
+        case 50:
+
+            quotes = "Hello My name is Inigo Montoya You killed my father Prepare to die <br>:The Princess Bride";
+            break;
+        case 51:
+
+            quotes = "I am your father <br>:Star Wars Episode V";
+            break;
+        case 52:
+
+            quotes = "The first rule of Fight Club is: You do not talk about Fight Club <br>:Fight Club";
+            break;
+        case 1:
+
+            quotes = "There's no place like home <br>:The Wizard of Oz";
+            break;
+        case 53:
+
+            quotes = "You talkin' to me? <br>:Taxi Driver";
+            break;
+        case 54:
+
+            quotes = "Toto, I've a feeling we're not in Kansas anymore<br> :The Wizard of Oz";
+            break;
+        case 55:
+
+            quotes = "May the Force be with you <br>:Star Wars";
+            break;
+        case 56:
+
+            quotes = "You're gonna need a bigger boat <br>:Jaws";
+            break;
+        case 57:
+
+            quotes = "May the odds be ever in your favor <br>:Hunger Games ";
+            break;
+
+    }
+
+    // Display the word inside the text box
+    console.log(quotes);
+    document.getElementById("textbox").innerHTML = quotes;
+}
